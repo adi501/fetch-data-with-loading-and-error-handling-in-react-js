db7dd79ad654a5933a1f6aad32b6a7228b291683
Adinarayana Reddy JC <jc.adi101@gmail.com>
2020-04-06 18:17:42 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# fetch-data-with-loading-and-error-handling-in-react-js
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import FetchDataLoadingErrorHandling from "./Components/FetchDataLoadingErrorHandling";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FetchDataLoadingErrorHandling />
+    </div>
+  );
+}
